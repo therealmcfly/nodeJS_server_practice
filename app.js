8019baaf00e9a8e08a183d63c98c78127ab06aeb
therealmcfly <eugene.fly.lee@gmail.com>
2022-09-17 13:04:01 +0900

    changed the content-type of the response to html instead of plain text

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("server is running");
-  res.end("Welcome to My Home Page!");
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>Welcome to My Home Page!</h1>");
+  res.end();
 });
 
 server.listen(5000);
