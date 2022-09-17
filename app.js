e7a49287601b855843697ae462acfe7f4d452578
therealmcfly <eugene.fly.lee@gmail.com>
2022-09-17 13:24:50 +0900

    added response for /about url and 404 response for invalid or not existing url

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("server is running");
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Welcome to My Home Page!</h1>");
-  res.end();
+  const url = req.url;
+  // home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Welcome to My Home Page!</h1>");
+    res.end();
+  }
+  // about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>This is the about page</h1>");
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, "Invalid Path", { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
