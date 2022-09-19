df00c52bc309b24b70bcc7536502034efabff464
therealmcfly <eugene.fly.lee@gmail.com>
2022-09-19 16:59:38 +0900

    added querying to server app

--- app.js
+++ app.js
@@ -21,6 +21,24 @@ app.get("/api/products/:productID", (req, res) => {
   res.json(singleProduct);
 });
 
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    return res.status(200).send("no products match the search value");
+    // return res.status(200).json({ success: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
+});
+
 app.all("*", (req, res) => {
   res.status(400).send("<h1>Invalid Path</h1>");
 });
