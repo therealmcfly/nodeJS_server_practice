52e37c422ddb9b09dfd209649b8394fd5ae1c0d6
therealmcfly <eugene.fly.lee@gmail.com>
2022-09-20 18:43:54 +0900

    Created a simple frontend page to send post method request to server, added response for post request in server app. Used express built-in middlewares(urlencoded, json).

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 const express = require("express");
 const app = express();
 
+// made middleware
 const logger = require("./logger");
 const authorize = require("./authorize");
 
-const { products } = require("./public/data");
+const { products, people } = require("./public/data");
 const newProducts = products.map((product) => {
   const { id, name, price } = product;
   return { id, name, price };
 });
 
+//static assets middleware
 app.use([express.static("./public"), logger]);
+// parse form data middleware
+app.use(express.urlencoded({ extended: false }));
+// app.use("/api", authorize);
+// parse json middleware
+app.use(express.json());
 
-app.use("/api", authorize);
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res
+      .status(400)
+      .json({ success: false, msg: "please provide a name value" });
+  }
+  res.status(201).send({ success: true, person: name });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    res.send(`Welcome ${name}`);
+  } else {
+    res.status(404).send("Please Provide a Name");
+  }
+});
 
 app.get("/api/products", (req, res) => {
   res.json(newProducts);
