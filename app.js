f6183be2d52618632da0da5cd6d7359bcbf93097
therealmcfly <eugene.fly.lee@gmail.com>
2022-09-21 13:14:05 +0900

    added functions in server app that handles POST, PUT, DELETE requests to api/people route. practiced requesting through postman app.

--- app.js
+++ app.js
@@ -5,32 +5,21 @@ const app = express();
 const logger = require("./logger");
 const authorize = require("./authorize");
 
-const { products, people } = require("./public/data");
+let { products, people } = require("./public/data");
 const newProducts = products.map((product) => {
   const { id, name, price } = product;
   return { id, name, price };
 });
 
+//created middleware
+app.use(logger);
+// app.use("/api", authorize);
 //static assets middleware
-app.use([express.static("./public"), logger]);
+app.use(express.static("./public"));
 // parse form data middleware
 app.use(express.urlencoded({ extended: false }));
-// app.use("/api", authorize);
 // parse json middleware
 app.use(express.json());
-
-app.get("/api/people", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
-app.post("/api/people", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    res
-      .status(400)
-      .json({ success: false, msg: "please provide a name value" });
-  }
-  res.status(201).send({ success: true, person: name });
-});
 
 app.post("/login", (req, res) => {
   const { name } = req.body;
@@ -39,6 +28,95 @@ app.post("/login", (req, res) => {
   } else {
     res.status(404).send("Please Provide a Name");
   }
+});
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res
+      .status(400)
+      .json({ success: false, msg: "please provide a name value" });
+    console.log("Post Failed : Name value was not submited");
+  } else {
+    const newPeople = [
+      ...people,
+      {
+        id: people.length + 1,
+        name,
+      },
+    ];
+    people = newPeople;
+    res.status(201).send({ success: true, data: people });
+    console.log("Post Successful");
+  }
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  if (!people.find((person) => person.id == Number(id))) {
+    res.status(400).json({
+      success: false,
+      msg: `The person id '${Number(id)}' does not exist in the people data`,
+    });
+    console.log(`PUT Failed : id '${Number(id)}' does not exist`);
+  } else if (!name) {
+    res.status(400).json({
+      success: false,
+      msg: `Please input a name value to update`,
+    });
+    console.log(`PUT Failed : A name value to update was not provided`);
+  } else {
+    const updatedPeople = people.map((person) => {
+      if (person.id === Number(id)) {
+        const updatedPerson = { ...person, name };
+        return updatedPerson;
+      } else {
+        return person;
+      }
+    });
+    people = updatedPeople;
+    console.log(updatedPeople);
+    res.status(201).send({ success: true, data: people });
+    console.log(
+      `PUT Successful : Updated name value of person id '${Number(
+        id
+      )}' to "${name}"`
+    );
+  }
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  if (!people.find((person) => person.id == Number(id))) {
+    res.status(400).json({
+      success: false,
+      msg: `The person id '${Number(id)}' does not exist in the people data`,
+    });
+    console.log(`DELETE Failed : id '${Number(id)}' does not exist`);
+  } else {
+    const updatedPeople = people.filter((person) => person.id !== Number(id));
+    people = updatedPeople;
+    console.log(updatedPeople);
+    res.status(201).send({ success: true, data: people });
+    console.log(`DELETE Successful : Deleted person id '${Number(id)}'`);
+  }
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = newProducts.find(
+    (product) => product.id === Number(productID)
+  );
+  res.json(singleProduct);
 });
 
 app.get("/api/products", (req, res) => {
